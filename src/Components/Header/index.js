6c2ea032e585840d2466fad83a819ff339050c06
Mohamed Adnan <adnansumi98@gmail.com>
2024-09-02 12:50:54 +0530

    used context in home

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,6 @@
-import {AiOutlineShoppingCart, AiOutlineLogout} from 'react-icons/ai'
-import {useContext} from 'react/cjs/react.production.min'
+import {useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {AiOutlineShoppingCart, AiOutlineLogout} from 'react-icons/ai'
 import Cookies from 'js-cookie'
 import {CartContext} from '../../Utility/CartContext'
 import {NameContext} from '../../Utility/NameContext'
